3a302a3842f2712d36188e0a03d8cd18f709a11b
19933jitu <jitumm390@gmail.com>
2021-05-22 11:11:06 +0600

    change for

--- src/app/employee/salary-sheet/salary-sheet.component.ts
+++ src/app/employee/salary-sheet/salary-sheet.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { EmployeeService } from '../_core/service/employee.service';
+import { GradeService } from '../_core/service/grade.service';
 
 @Component({
   selector: 'app-salary-sheet',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalarySheetComponent implements OnInit {
 
-  constructor() { }
+
+  employeeList: any = [];
+
+  constructor(
+    private gradeService: GradeService,
+    private employeeService: EmployeeService,
+    private toastr: ToastrService,
+  ) { }
 
   ngOnInit() {
+    this.findEmpList();
+  }
+
+  findEmpList() {
+    this.employeeService.findEmpList().subscribe(
+      res => {
+        if (res.success && res.items) {
+          this.employeeList = res.items;
+        }
+      },
+      err => {
+        console.log('grade list err', err);
+      }
+    );
+  }
+
+  totalSalary() {
+    return this.employeeList.map(data => data.grade.salary).reduce((data2, data3) => { return data2 + data3 }, 0);
   }
 
 }
